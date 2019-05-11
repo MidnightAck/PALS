74b5dfa1166d1064d8ab69a6de4927be2ca3bffe
CrowFea <wendaoliu40@outlook.com>
2019-05-11 13:24:52 +0800

    upload v1

---
+++ miniprogram/pages/taskinf/taskinf.js
@@ -0,0 +1,76 @@
+var app = getApp()
+const db = wx.cloud.database();
+//wx.cloud.init();
+Page({
+  data: {
+    index: 0,
+    stuID: '',
+    category: ['比赛', '项目', '拼车', '其他'],
+    taskOngoing: {
+      Giverid: '',
+      Reciverid: '',
+      _id: '',
+      briefInf: '',
+      category: '',
+      taskid: ''
+    }
+
+  },
+  onLoad: function(option) {
+
+    this.setData({
+      index: option.index,
+      stuID: app.globalData.stuId,
+      taskOngoing: JSON.parse(option.taskongoing)
+    })
+    //console.log(this.data.index)
+  },
+  onHide: function() {
+    console.log("taskinf page onhide")
+  },
+  update:function(event){
+    var that = this
+    //console.log(that.data.taskOngoing[event.currentTarget.dataset.index])
+    wx.navigateTo({
+      url: '../taskinf_change/taskinf_change?taskongoing=' + JSON.stringify(that.data.taskOngoing),
+      success: function (res) { },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+
+
+
+
+  },
+
+  dismiss: function() {
+    var that = this;
+    wx.showModal({
+      title: '',
+      content: '一旦解散队伍将不能再恢复',
+      confirmText: "确认解散",
+      cancelText: "手抖",
+      success: function(res) {
+        console.log(res);
+        if (res.confirm) {
+          db.collection('taskOngoing').doc(that.data.taskOngoing._id).remove({
+            success(res) {
+              wx.showToast({
+                title: '解散成功！',
+                icon: 'success',
+                duration: 2000
+              });
+            }
+          })
+          wx.hideToast();
+          wx.switchTab({
+            url: '../usercenter/usercenter'
+          })
+        } else {
+          console.log('用户手抖了')
+        }
+      }
+    });
+
+  }
+});
---
+++ miniprogram/pages/usercenter/usercenter.js
@@ -0,0 +1,70 @@
+var app = getApp()
+//wx.cloud.init();
+
+Page({
+  data: {
+    category: ['比赛', '项目', '拼车', '其他'],
+    taskOngoing: [{
+      Giverid: '',
+      Reciverid: '',
+      _id: '',
+      briefInf: '',
+      category: '',
+      taskid: ''
+    }]
+  },
+  onShow: function() {
+    var that = this
+    const db = wx.cloud.database();
+    const _ = db.command
+    db.collection('taskOngoing').where(_.or([{
+        Giverid: app.globalData.stuId
+      },
+      {
+        Reciverid: app.globalData.stuId
+      }
+    ])).get({
+      success: res => {
+        console.log(res.data)
+        let taskOngoing = res.data;
+        if (taskOngoing) {
+          this.setData({
+            taskOngoing: taskOngoing
+          })
+        }
+      },
+      fail(res) {
+        console.log(fail)
+      }
+    })
+  },
+  detailedInf: function(event) {
+    var that = this
+    console.log(that.data.taskOngoing[event.currentTarget.dataset.index])
+    wx.navigateTo({
+      url: '../taskinf/taskinf?index=' + event.currentTarget.dataset.index + '&taskongoing=' + JSON.stringify(that.data.taskOngoing[event.currentTarget.dataset.index]),
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+
+  },
+  onHide: function() {
+    console.log("usercenter page onhide")
+  },
+  userinf: function() {
+    wx.navigateTo({
+      url: '../userinf/userinf',
+      success: function(res) {
+        "login relaunch success"
+      },
+      fail: function(res) {
+        "login relaunch fail"
+      },
+      complete: function(res) {
+        "login relaunch complete"
+      },
+    })
+
+  }
+});
