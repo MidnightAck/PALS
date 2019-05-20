3484426e4c20710375f2217eb4e56798ecd4ff92
CrowFea <wendaoliu40@outlook.com>
2019-05-20 20:36:26 +0800

    message toast

--- miniprogram/pages/candiinf/candiinf.js
+++ miniprogram/pages/candiinf/candiinf.js
@@ -53,56 +53,100 @@ Page({
 
  
   button_two(e) {
-    var candiopenid=this.data.user._openid
-    console.log(candiopenid)
-    let week = new Date() - (1000 * 60 * 60 * 24 * 7) //建立7天时间戳
-    //储存formId，并打时间戳
-    db.collection('formId').add({
-      data: {
-        openid: wx.getStorageSync("openid"),
-        formId: e.detail.formId,
-        date: (new Date()).valueOf()
+    var candiopenid = this.data.user._openid
+    var wxid = this.data.user.wxid
+    wx.showModal({
+      title: '',
+      content: '确认选择他吗，我们提醒候选人，并将他的微信号复制到剪贴板',
+      confirmText: "那当然",
+      cancelText: "再想想",
+      success: function (res) {
+        console.log(res)
+        if (res.confirm) {
+          wx.setClipboardData({
+            data: wxid
+          })
+          
+          console.log(candiopenid)
+          let week = new Date() - (1000 * 60 * 60 * 24 * 7) //建立7天时间戳
+          //储存formId，并打时间戳
+          db.collection('formId').add({
+            data: {
+              openid: wx.getStorageSync("openid"),
+              formId: e.detail.formId,
+              date: (new Date()).valueOf()
+            }
+          })
+            .then(res => {
+              console.log(res)
+            })
+          //获取formId数据 
+          db.collection('formId').where({
+            _openid: candiopenid,
+            date: _.gt(week) //获取7天内
+          }).get().then(res => {
+            console.log(res.data)
+            var formIdList = res.data
+            let date = new Date();
+            let data = JSON.stringify({
+              "keyword1": {
+                "value": "组队已确认！"
+              },
+              "keyword2": {
+                "value": date
+              }
+            })
+            //调用云函数发送模版消息
+            wx.cloud.callFunction({
+              name: 'moban',
+              data: {
+                openid: formIdList[0].openid,
+                template_id: "tckUPjs60Zy94Ixg9ZBiqPgfhQn24_ZdV0b-WoOKFdY",
+                // page: "/pages/fromID/index?sender_openid=" + wx.getStorageSync("openid") + "&value=" + value, //携带参数
+                form_id: formIdList[0].formId,
+                data,
+                emphasis_keyword: "keyword1.DATA"
+              },
+              success: res => {
+                console.log('模版消息发送成功: ', res)
+                var id = formIdList[0]._id
+                wx.cloud.callFunction({
+                  name: 'remove',
+                  data: {
+                    id,
+                  },
+                  success: res => {
+                    console.log('删除成功：', res)
+                    if (res.result.stats.removed == 1) {
+                      wx.showToast({
+                        title: '删除formId成功',
+                      })
+                    }
+                  },
+                  fail: err => {
+                    console.log('删除失败：', err)
+                  }
+                })
+              },
+              fail: err => {
+                console.error('模版消息发送失败：', err)
+              }
+            })
+          })
+          wx.showModal({
+            title: '申请成功',
+            content: '我们已经告诉发起人啦',
+            showCancel: false,
+            confirmText: '确认'
+          })
+        }
+        else {
+          console.log('用户手抖了')
+        }
       }
     })
-      .then(res => {
-        console.log(res)
-      })
-    //获取formId数据 
-    db.collection('formId').where({
-      _openid: candiopenid,
-      date: _.gt(week) //获取7天内
-    }).get().then(res => {
-      console.log(res.data)
-      var formIdList = res.data
-      let date = new Date();
-      let data = JSON.stringify({
-        "keyword1": {
-          "value": "组队已确认！"
-        },
-        "keyword2": {
-          "value": date
-        }
-      })
-      //调用云函数发送模版消息
-      wx.cloud.callFunction({
-        name: 'moban',
-        data: {
-          openid: formIdList[0].openid,
-          template_id: "tckUPjs60Zy94Ixg9ZBiqPgfhQn24_ZdV0b-WoOKFdY",
-         // page: "/pages/fromID/index?sender_openid=" + wx.getStorageSync("openid") + "&value=" + value, //携带参数
-          form_id: formIdList[0].formId,
-          data,
-          emphasis_keyword: "keyword1.DATA"
-        },
-        success: res => {
-          console.log('模版消息发送成功: ', res)
-          this.remove(formIdList[0]._id); //调用删除formId函数
-        },
-        fail: err => {
-          console.error('模版消息发送失败：', err)
-        }
-      })
-    })
+
+   
   },
   ///////////////////存储formid/////////////
   button_three(e) {
